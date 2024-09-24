31e117c3899706eb677fcc7e7a5a93f641ffac50
Bianca Angelis <diangelis879@outlook.com>
2024-09-24 17:14:53 -0300

    (feat) working code for jpg to png converter

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -3,9 +3,23 @@ import os
 from PIL import Image
 
 # grab first and second argument
+origin_folder = sys.argv[1]
+destination_folder = sys.argv[2]
 
-# check if new folder exists, if not, create 
+# check if new folder exists, if not, create
+if os.path.exists(destination_folder) != True:
+  os.makedirs(destination_folder)
 
 # loop through pokedex
 # convert images to png
 # save to new folder
+
+for pokemon in os.listdir(origin_folder):
+  name, old_format = os.path.splitext(pokemon)
+  transformed = name + ".png"
+
+  try:
+    with Image.open(os.path.join(origin_folder,pokemon)) as im:
+      im.save(f"{os.path.join(destination_folder,transformed)}", "PNG")
+  except:
+    print(f"Conversion error on {name}")
