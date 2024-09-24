1454732b2a60eec736dda798549d2d3b9ae7cc3d
Bianca Angelis <diangelis879@outlook.com>
2024-09-24 16:11:56 -0300

    (task) delete solution files to start clean

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -2,17 +2,10 @@ import sys
 import os
 from PIL import Image
 
-path = sys.argv[1]
-directory = sys.argv[2]
+# grab first and second argument
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
-    
+# check if new folder exists, if not, create 
 
-for filename in os.listdir(path):
-  clean_name = os.path.splitext(filename)[0]
-  img = Image.open(f'{path}{filename}')
-  #added the / in case user doesn't enter it. You may want to check for this and add or remover it. 
-  img.save(f'{directory}/{clean_name}.png', 'png')
-  print('all done!')
-
+# loop through pokedex
+# convert images to png
+# save to new folder
